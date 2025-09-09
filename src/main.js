334de9f090f4ddf36fa99b0570484a4086719a3a
ShehryarAdil <shehryaradil360@gmail.com>
2025-09-09 20:54:53 +0500

    hidding firebase api key

--- src/main.js
+++ src/main.js
@@ -8,13 +8,13 @@ import { getFirestore, doc, setDoc, getDoc, collection, getDocs, addDoc, updateD
 
 // --- 1. PASTE YOUR FIREBASE CONFIGURATION HERE ---
 const firebaseConfig = {
-  apiKey: "AIzaSyBCB9Vk_N5p0qNlzzEZbd3txvP7SW7bMVo",
-  authDomain: "finvaultapp.firebaseapp.com",
-  projectId: "finvaultapp",
-  storageBucket: "finvaultapp.firebasestorage.app",
-  messagingSenderId: "384326381113",
-  appId: "1:384326381113:web:a5341f1961dcbd77fc67e8",
-  measurementId: "G-ZV817H8CTC"
+    apiKey: import.meta.env.VITE_Firebase_apikey,
+    authDomain: import.meta.env.VITE_Firebase_authDomain,
+    projectId: import.meta.env.VITE_Firebase_projectId,
+    storageBucket: import.meta.env.VITE_Firebase_storageBucket,
+    messagingSenderId: import.meta.env.VITE_Firebase_messagingSenderId,
+    appId: import.meta.env.VITE_Firebase_appId,
+    measurementId: import.meta.env.VITE_Firebase_measurementId
 };
 // ---------------------------------------------
 
